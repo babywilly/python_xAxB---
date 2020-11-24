e16b32eb4df5f5f4511a390e6f313222789d6f7f
寶群 <73570590+babywilly@users.noreply.github.com>
2020-11-24 22:08:09 +0800

    註解

--- 5.py
+++ 5.py
@@ -6,22 +6,20 @@ a_count=0   #使A、B的數量都先為0
 b_count=0
 for i in range(4):
     answer+=str(items[i])
-
 while(True):
     guessnumber=input('請輸入4個不同的數字: ')
     if not guessnumber.isdigit():   #判斷輸入是否為4位數字
         pass
-    elif guessnumber
     else:
         if guessnumber==answer:
             print('恭喜你答對')
             break
-        for i in range(4):
+        for i in range(4):                               #比較兩個數
             for j in range(4):
-                if i==j and guessnumber[i]==answer[j]:
+                if i==j and guessnumber[i]==answer[j]:   #位子一樣、數字一樣
                     a_count+=1
-                elif guessnumber[i]==answer[j]:
+                elif guessnumber[i]==answer[j]:          #有同樣的數字
                     b_count+=1
-        print(a_count,'A',b_count,'B')
+        print(a_count,'A',b_count,'B')            #輸出xAxB
         a_count=0
         b_count=0
